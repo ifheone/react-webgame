0482be24debb8de79cc45a42331a9c0b1e6f9aac
jangheon Pyo <ifheone@gmail.com>
2023-01-16 22:45:58 +0900

    구구단 웹팩 수정

--- 1.구구단/webpack.config.js
+++ 1.구구단/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { webpack } = require("webpack");
 
 module.exports = {
   mode: "development",
@@ -15,12 +16,23 @@ module.exports = {
         test: /\.jsx?$/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 5% in KR", "last 2 chrome versions"], //browserslist(옵션 사이트)
+                },
+              },
+            ],
+            "@babel/preset-react",
+          ],
           plugins: [],
         },
       },
     ],
   },
+  //plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
   output: {
     filename: "app.js",
     path: path.join(__dirname, "dist"),
