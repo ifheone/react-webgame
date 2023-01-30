6680f0b0309044fecbecdfcf561e2424b0017aeb
jangheon Pyo <ifheone@gmail.com>
2023-01-30 17:06:10 +0900

    3.숫자야구 - class

---
+++ 3.숫자야구/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+module.exports = {
+  name: "wordrelay-setting",
+  mode: "development", // 실제서비스시 : production
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  entry: {
+    app: ["./client"],
+  }, //입력
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 5% in KR", "last 2 chrome versions"], //browserslist(옵션 사이트)
+                },
+              },
+            ],
+            "@babel/preset-react",
+          ],
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "react-refresh/babel",
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [new RefreshWebpackPlugin()],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+    publicPath: "/dist",
+  }, //출력
+  devServer: {
+    devMiddleware: { publicPath: "/dist" },
+    static: { directory: path.resolve(__dirname) },
+    hot: true,
+  },
+};
