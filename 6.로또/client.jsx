46e50416df671f33b49ad96aaed76ffda9dc538a
jangheon Pyo <ifheone@gmail.com>
2023-02-13 16:44:23 +0900

    6.로또 - hooks

--- 6.로또/client.jsx
+++ 6.로또/client.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Lotto from "./LottoClass";
+import Lotto from "./Lotto";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(<Lotto />);
